800f6ca80b6abf7e63c06e412856ffd568d4efb2
Nikita Shpilevoy <nick@shpil.io>
2020-12-21 13:54:45 +0300

    More logging

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,9 +15,9 @@ wget -O ~/.aws/config http://secrets.local.shpil.dev/aws/config
 wget -O ~/.aws/credentials http://secrets.local.shpil.dev/aws/credentials
 
 echo '[Debug] Push files to AWS S3'
-aws s3 sync $source $target --delete --acl public-read
+(set -x; aws s3 sync $source $target --delete --acl public-read)
 
 echo '[Debug] Regenerate '
-aws cloudfront create-invalidation --distribution-id $cloudfront --paths '/*'
+(set -x; aws cloudfront create-invalidation --distribution-id $cloudfront --paths '/*')
 
 echo '[Info] Done.'
