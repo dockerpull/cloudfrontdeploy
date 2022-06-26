7fbedfe012a5312cb7a1d66ff7b4a67aa13ac5cc
Nikita Shpilevoy <nick@shpil.io>
2022-06-26 18:59:55 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,11 +9,6 @@ cloudfront=$3
 export commit=$GITHUB_SHA
 export branch=${GITHUB_REF##*/}
 
-echo '[Debug] Authenticate in AWS'
-mkdir -p ~/.aws
-wget -O ~/.aws/config http://secrets.local.shpil.dev/aws/config
-wget -O ~/.aws/credentials http://secrets.local.shpil.dev/aws/credentials
-
 echo '[Debug] Push files to AWS S3'
 (set -x; aws s3 sync $source $target --delete)
 
