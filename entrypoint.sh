adbfb1f16f77d534710b9b7f69c6f64aea71783b
Nikita Shpilevoy <nick@shpil.io>
2022-06-26 20:38:05 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,10 +9,12 @@ cloudfront=$3
 export commit=$GITHUB_SHA
 export branch=${GITHUB_REF##*/}
 
-echo '[Debug] Push files to AWS S3'
+echo [Debug] Push files to AWS S3 $target
 (set -x; aws s3 sync $source $target --delete)
 
-echo '[Debug] Regenerate '
+echo [Debug] Regenerate $cloudfront
+echo SECRET AWS_ACCESS_KEY_ID $AWS_ACCESS_KEY_ID
+echo SECRET AWS_SECRET_ACCESS_KEY $AWS_SECRET_ACCESS_KEY
 (set -x; aws cloudfront create-invalidation --distribution-id $cloudfront --paths '/*')
 
 echo '[Info] Done.'
